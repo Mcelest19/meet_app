0b40940cf44a25f64a0a7156dd6a3bb6c6343834
Mcelest19 <mrn.celestino@gmail.com>
2023-05-02 19:29:50 +0200

    added accessToken

--- auth-server/handler.js
+++ auth-server/handler.js
@@ -22,7 +22,7 @@ const credentials = {
   redirect_uris: ["https://mcelest19.github.io/meet_app"],
   javascript_origins: ["https://mcelest19.github.io", "http://localhost:3000"],
 };
-const { client_secret, client_id, redirect_uris} = credentials;
+const { client_secret, client_id, redirect_uris, calendar_id } = credentials;
 const oAuth2Client = new google.auth.OAuth2(
   client_id,
   client_secret,
@@ -55,8 +55,52 @@ module.exports.getAuthURL = async () => {
       "Access-Control-Allow-Origin": "*",
       "Access-Control-Allow-Credentials": true,
     },
-    body:({
+    body: JSON.stringify ({
       authUrl: authUrl,
     }),
   };
 };
+
+module.exports.getAccessToken =  async (event) => {
+  // The values used to instantiate the OAuthClient are at the top of the file
+    const oAuth2Client = new google.auth.OAuth2(
+      client_id,
+      client_secret,
+      redirect_uris[0]
+    );
+    // Decode authorization code extracted from the URL query
+    const code = decodeURIComponent(`${event.pathParameters.code}`);
+  
+    return new Promise((resolve, reject) => {
+      /**
+       *  Exchange authorization code for access token with a “callback” after the exchange,
+       *  The callback in this case is an arrow function with the results as parameters: “err” and “token.”
+       */
+  
+      oAuth2Client.getToken(code, (err, token) => {
+        if (err) {
+          return reject(err);
+        }
+        return resolve(token);
+      });
+    })
+      .then((token) => {
+        // Respond with OAuth token 
+        return {
+          statusCode: 200,
+          headers: {
+            "Access-Control-Allow-Origin": "*",
+            "Access-Control-Allow-Credentials": true,
+          },
+          body: JSON.stringify(token),
+        };
+      })
+      .catch((err) => {
+        // Handle error
+        console.error(err);
+        return {
+          statusCode: 500,
+          body: JSON.stringify(err),
+        };
+      });
+  };
