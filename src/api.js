31a45197edb13bbe47034c65b76d52237b0f2e8b
Mcelest19 <mrn.celestino@gmail.com>
2023-05-12 13:26:57 +0200

    fixed axios

--- src/api.js
+++ src/api.js
@@ -1,13 +1,19 @@
+/**
+ *
+ * @param {*} events:
+ * The following function should be in the “api.js” file.
+ * This function takes an events array, then uses map to create a new array with only locations.
+ * It will also remove all duplicates by creating another new array using the spread operator and spreading a Set.
+ * The Set will remove all duplicates from the array.
+ */
+
 import { mockData } from './mock-data';
-import NProgress from "nprogress";
-import axios from "axios";
-
-
+import axios from 'axios';
+import NProgress from 'nprogress';
 
 export const getAccessToken = async () => {
   const accessToken = localStorage.getItem('access_token');
   const tokenCheck = accessToken && (await checkToken(accessToken));
-
   if (!accessToken || tokenCheck.error) {
     await localStorage.removeItem("access_token");
     const searchParams = new URLSearchParams(window.location.search);
@@ -22,30 +28,28 @@ export const getAccessToken = async () => {
     return code && getToken(code);
   }
   return accessToken;
+}
+
+const checkToken = async (accessToken) => {
+  const result = await fetch(
+    `https://www.googleapis.com/oauth2/v1/tokeninfo?access_token=${accessToken}`
+  )
+    .then((res) => res.json())
+    .catch((error) => error.json());
+
+  return result;
 };
-
-
-  const checkToken = async (accessToken) => {
-    const result = await fetch(
-      `https://www.googleapis.com/oauth2/v1/tokeninfo?access_token=${accessToken}`
-    )
-      .then((res) => res.json())
-      .catch((error) => error.json());
-  
-    return result;
-  };
 
 export const getEvents = async () => {
   NProgress.start();
 
-  if (window.location.href.startsWith("http://localhost")) {
+  if (window.location.href.startsWith('http://localhost')) {
     NProgress.done();
     return mockData;
   }
 
   const token = await getAccessToken();
-
-  if (token) {
+    if (token) {
     removeQuery();
     const url = 'https://3nd1jur404.execute-api.eu-central-1.amazonaws.com/dev/api/get-events' + '/' + token;
     const result = await axios.get(url);
@@ -87,11 +91,13 @@ const getToken = async (code) => {
   } catch(error) {
       error.json();
   }
-}
+};
 
 export const extractLocations = (events) => {
   var extractLocations = events.map((event) => event.location);
   var locations = [...new Set(extractLocations)];
   return locations;
 };
+
+
 
